6cf2c43d002249866336dde8f1e6d4d03da9b181
Administrador <Administrador@IDATCOMPUTACION>
2019-11-20 22:42:46 -0500

    clase9

---
+++ SpringMVCWebApp2/src/main/java/idat/edu/pe/daa2/SpringMvcWebApp2Application.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWebApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcWebApp2Application.class, args);
+	}
+
+}
---
+++ SpringMVCWebApp2/src/test/java/idat/edu/pe/daa2/SpringMvcWebApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.edu.pe.daa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcWebApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
